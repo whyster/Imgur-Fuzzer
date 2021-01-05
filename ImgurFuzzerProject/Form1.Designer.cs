5059c74b2205ef90a8a272b1385ab17b5a44f180
whyster <aleksey.kalinovski@gmail.com>
2021-01-04 23:21:36 -0500

    Existing Project To Git

---
+++ ImgurFuzzerProject/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace ImgurFuzzerProject {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.imgurPreview = new System.Windows.Forms.PictureBox();
+            this.containerControl1 = new System.Windows.Forms.ContainerControl();
+            this.saveFileLabel = new System.Windows.Forms.Label();
+            this.saveFileLocation = new System.Windows.Forms.TextBox();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.startButton = new System.Windows.Forms.Button();
+            this.logBox = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.stopButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize) (this.imgurPreview)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgurPreview
+            // 
+            this.imgurPreview.Location = new System.Drawing.Point(12, 35);
+            this.imgurPreview.Name = "imgurPreview";
+            this.imgurPreview.Size = new System.Drawing.Size(205, 169);
+            this.imgurPreview.TabIndex = 0;
+            this.imgurPreview.TabStop = false;
+            // 
+            // containerControl1
+            // 
+            this.containerControl1.Location = new System.Drawing.Point(0, 0);
+            this.containerControl1.Name = "containerControl1";
+            this.containerControl1.Size = new System.Drawing.Size(0, 0);
+            this.containerControl1.TabIndex = 0;
+            this.containerControl1.Text = "containerControl1";
+            // 
+            // saveFileLabel
+            // 
+            this.saveFileLabel.Location = new System.Drawing.Point(12, 9);
+            this.saveFileLabel.Name = "saveFileLabel";
+            this.saveFileLabel.Size = new System.Drawing.Size(99, 23);
+            this.saveFileLabel.TabIndex = 2;
+            this.saveFileLabel.Text = "Save File Location:";
+            this.saveFileLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // saveFileLocation
+            // 
+            this.saveFileLocation.Location = new System.Drawing.Point(117, 6);
+            this.saveFileLocation.Name = "saveFileLocation";
+            this.saveFileLocation.ReadOnly = true;
+            this.saveFileLocation.Size = new System.Drawing.Size(284, 20);
+            this.saveFileLocation.TabIndex = 1;
+            this.saveFileLocation.Click += new System.EventHandler(this.OpenSaveFileDialog);
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.DefaultExt = "json";
+            this.saveFileDialog.Filter = "Json files (*.json)|*.json|All files (*.*)|*.*;";
+            this.saveFileDialog.RestoreDirectory = true;
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.startButton.Location = new System.Drawing.Point(223, 35);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(178, 29);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "Start Fuzzing";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.StartFuzzing);
+            // 
+            // logBox
+            // 
+            this.logBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.logBox.Location = new System.Drawing.Point(223, 70);
+            this.logBox.Multiline = true;
+            this.logBox.Name = "logBox";
+            this.logBox.Size = new System.Drawing.Size(178, 42);
+            this.logBox.TabIndex = 4;
+            this.logBox.Text = "AAAAAAA";
+            this.logBox.TextChanged += new System.EventHandler(this.TextScroll);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.StartFuzzingBackground);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Enabled = false;
+            this.stopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.stopButton.Location = new System.Drawing.Point(223, 118);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(178, 29);
+            this.stopButton.TabIndex = 5;
+            this.stopButton.Text = "Stop Fuzzing";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.StopFuzzingWorker);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 221);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.saveFileLabel);
+            this.Controls.Add(this.saveFileLocation);
+            this.Controls.Add(this.imgurPreview);
+            this.MaximumSize = new System.Drawing.Size(429, 260);
+            this.MinimumSize = new System.Drawing.Size(429, 260);
+            this.Name = "Form1";
+            this.Text = "I.Imgur Fuzzer";
+            ((System.ComponentModel.ISupportInitialize) (this.imgurPreview)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button stopButton;
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+
+        private System.Windows.Forms.PictureBox imgurPreview;
+        private System.Windows.Forms.Label saveFileLabel;
+
+        private System.Windows.Forms.TextBox logBox;
+        private System.Windows.Forms.TextBox saveFileLocation;
+        private System.Windows.Forms.Button startButton;
+
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+
+        private System.Windows.Forms.ContainerControl containerControl1;
+
+        #endregion
+    }
+}
---
+++ ImgurFuzzerProject/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Tracing;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Windows.Forms;
+
+namespace ImgurFuzzerProject {
+    public partial class Form1 : Form {
+        // Valid characters for imgur decorator
+        public string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                                   "abcdefghijklmnopqrstuvwxyz" +
+                                   "0123456789";
+        // Webclient is space intensive, beneficial for it to be global
+        public HttpClient webClient = new HttpClient();
+
+        public Form1() {
+            InitializeComponent();
+            // To make requests to imgur, we need a user-agent flag for the request to not fail
+            webClient.DefaultRequestHeaders.Add("User-Agent", "Imgur-Fuzzer: Tool");
+
+            //Make save file dialog show up in installation path
+            saveFileDialog.InitialDirectory = Application.StartupPath;
+        }
+
+        // Save file dialog event handler
+        private void OpenSaveFileDialog(object sender, EventArgs e) {
+            saveFileDialog.ShowDialog();
+            saveFileLocation.Text = saveFileDialog.FileName;
+        }
+
+        // Start button event handler
+        private void StartFuzzing(object sender, EventArgs e) {
+            if (saveFileLocation.Text == "") {
+                errorProvider1.SetError(saveFileLocation, "Save file location required");
+            }
+            else {
+                //Backgroundworker as UI hangs if fuzzing is started synchronously
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        // Fuzzing method to be ran by backgroundworker1
+        private void StartFuzzingBackground(object sender, DoWorkEventArgs e) {
+            
+            /* Imgur image links have a 7 character long alphanumeric identifier
+             * E. G. https://i.imgur.com/ABC00F0/
+             */
+            string url = "https://i.imgur.com/";
+            int[] decoratorPos = new int[7]; // Initializing a decorator position array
+            
+            
+            for (int i = 0; i < 7; i++) {
+                char character = logBox.Text[i];
+                decoratorPos[i] = characters.IndexOf(character);
+            }
+            // Check if the saveFileLocation has been set
+            if(saveFileLocation.Text != "") {
+                startButton.Enabled = false; 
+                stopButton.Enabled = true;
+
+                /* Problem: To iterate through every possible imgur link, decorator must be iteratively stepped up
+                 * Solution: Use the decoratorPos array to select characters from the predetermined characters string
+                 * and add 1 to the [0] position. Once any [n] position in decoratorPos hits the length of characters
+                 * reset [n] to 0 and add 1 to [n+1] 
+                 */
+                while (decoratorPos[6] < characters.Length) {
+                    if (backgroundWorker1.CancellationPending) {
+                        e.Cancel = true;
+                        break;
+                    }
+
+                    string decorator = "";
+                    decoratorPos[0] += 1;
+                    // Iterate through all decoratorPositions and check if anypart hit the max
+                    for (int i = 0; i < decoratorPos.Length-1; ++i) {
+                        if (decoratorPos[i] >= characters.Length) {
+                            decoratorPos[i] = 0;
+                            decoratorPos[i + 1] += 1;
+                        }
+                    }
+
+                    /*construct a decorator from the positions
+                     * Python: ''.join([characters[num] for num in decoratorPos])
+                     */
+                    foreach (var num in decoratorPos) {
+                        decorator += characters[num];
+                    }
+
+                    logBox.Text = $"{decorator}";
+                    string result = CheckIfUrlValid($"{url}{decorator}.png");
+                    // Check if result is not a redirect
+                    if (result != "https://i.imgur.com/removed.png") {
+                        /* Storing data in memory to write all in one go is not a stable course of action
+                         * This writes the data immediately so no memory leak can take place
+                         */
+                        // File.AppendAllText(saveFileLocation.Text, $"\n\"{result}\",");
+                        File.AppendAllText(saveFileLocation.Text, $"\n{result}");
+                    }
+                }
+
+                
+                startButton.Enabled = true;
+                stopButton.Enabled = false;
+            }
+            else {
+                errorProvider1.SetError(saveFileLocation, "Save file location required");
+            }
+            
+            //CheckIfUrlValid("https://i.imgur.com/ka7Ln7w.png");
+            
+        }
+
+        /* CheckIfUrlValid
+         * Params: URL (A string representing a potential i.imgur image url)
+         * Returns the URL passed in, if the url is a valid imgur image
+         */
+        private string CheckIfUrlValid(string url) {
+            string imgDirect = "https://i.imgur.com/removed.png";
+            //Query the url
+            var res = webClient.GetAsync(url).Result;
+            //Get an image from the stream
+            Image image = Image.FromStream(res.Content.ReadAsStreamAsync().Result);
+            if (image != null) {
+                //If image exists, show it on the imgurPreview
+                imgurPreview.Image = image;
+            }
+            return (res.RequestMessage.RequestUri.AbsoluteUri == imgDirect) ? imgDirect : res.RequestMessage.RequestUri.AbsoluteUri;
+        }
+        // Move the cursor to the end of the logBox, so newest errors are constantly shown
+        private void TextScroll(object sender, EventArgs e) {
+            logBox.SelectionStart = logBox.Text.Length;
+            logBox.ScrollToCaret();
+        }
+
+        // Stop button event handler
+        private void StopFuzzingWorker(object sender, EventArgs e) {
+            stopButton.Enabled = false;
+            backgroundWorker1.CancelAsync();
+        }
+    }
+}
